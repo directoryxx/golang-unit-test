185fe8ea72695ea064d854a0f9e13ebd7b83bc45
directoryx <anggawijaya4567@gmail.com>
2021-06-28 14:33:33 +0700

    Test Skip

--- helper/HelloWorld_test.go
+++ helper/HelloWorld_test.go
@@ -3,11 +3,12 @@ package helper
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"runtime"
 	"testing"
 )
 
 func TestHelloWorld(t *testing.T) {
-	result := HelloWorld("Anga")
+	result := HelloWorld("Angga")
 	if result != "Hello Angga" {
 		t.Fatal("Result must 'Hello Angga'")
 	}
@@ -16,7 +17,7 @@ func TestHelloWorld(t *testing.T) {
 }
 
 func TestHelloWorld2(t *testing.T) {
-	result := HelloWorld("Anga")
+	result := HelloWorld("Angga")
 	if result != "Hello Angga" {
 		t.Error("Result must 'Hello Angga'")
 	}
@@ -24,7 +25,7 @@ func TestHelloWorld2(t *testing.T) {
 }
 
 func TestHelloWorldRequire(t *testing.T) {
-	result := HelloWorld("Anga")
+	result := HelloWorld("Angga")
 	// Dibawah require tidak dijalankan
 	// Ex: TestHelloWorld3
 	require.Equal(t, "Hello Angga",result,"Result must equal 'Hello Angga'")
@@ -32,6 +33,15 @@ func TestHelloWorldRequire(t *testing.T) {
 }
 
 func TestHelloWorld3(t *testing.T) {
+	result := HelloWorld("Angga")
+	assert.Equal(t, "Hello Angga",result,"Result must equal 'Hello Angga'")
+}
+
+func TestSkip(t *testing.T)  {
+	if runtime.GOOS == "darwin"{
+		t.Skip("Test tidak dijalankan di mac")
+	}
+
 	result := HelloWorld("Angga")
 	assert.Equal(t, "Hello Angga",result,"Result must equal 'Hello Angga'")
 }
