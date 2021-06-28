1b4ff79a4f467bdd170315634fad83e9b1f68953
directoryx <anggawijaya4567@gmail.com>
2021-06-28 14:30:15 +0700

    require and assert

--- helper/HelloWorld_test.go
+++ helper/HelloWorld_test.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -20,3 +22,18 @@ func TestHelloWorld2(t *testing.T) {
 	}
 	//fmt.Println("TestHelloWorld2 Success")
 }
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Anga")
+	// Dibawah require tidak dijalankan
+	// Ex: TestHelloWorld3
+	require.Equal(t, "Hello Angga",result,"Result must equal 'Hello Angga'")
+	//fmt.Println("TestHelloWorld2 Success")
+}
+
+func TestHelloWorld3(t *testing.T) {
+	result := HelloWorld("Angga")
+	assert.Equal(t, "Hello Angga",result,"Result must equal 'Hello Angga'")
+}
+
+
